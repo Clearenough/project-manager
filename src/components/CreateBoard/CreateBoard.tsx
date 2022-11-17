f6a1f7494d1dcc720dcb14c85e8babb121d8d443
pashasha <smogida1337@gmail.com>
2022-11-17 21:34:27 +0300

    feet: add board creation

--- src/components/CreateBoard/CreateBoard.tsx
+++ src/components/CreateBoard/CreateBoard.tsx
@@ -1,12 +1,14 @@
-import { FC } from "react";
-import { TextField, Button, Typography } from "@mui/material";
-import { useForm, SubmitHandler } from "react-hook-form";
+import { FC } from 'react';
+import { TextField, Button, Typography } from '@mui/material';
+import { useForm, SubmitHandler } from 'react-hook-form';
 
-import styles from './CreateBoard.module.scss'
+import styles from './CreateBoard.module.scss';
+import { api } from '../../services/api';
+import { IBoardCreate } from '../../@types/common';
 
 interface ICreateBoard {
-  name: string,
-  description: string,
+  name: string;
+  description: string;
 }
 
 // interface IPropsDiv {
@@ -14,68 +16,90 @@ interface ICreateBoard {
 // }
 
 interface IPropsButton {
-  handler: (event?: React.MouseEvent<HTMLButtonElement> | React.MouseEvent<HTMLDivElement>) => void,
+  handler: (event?: React.MouseEvent<HTMLButtonElement> | React.MouseEvent<HTMLDivElement>) => void;
 }
 
+type mouseEvent = React.MouseEvent<HTMLButtonElement> | React.MouseEvent<HTMLDivElement>;
 
-type mouseEvent = React.MouseEvent<HTMLButtonElement> | React.MouseEvent<HTMLDivElement>
-
-const CreateBoard: FC<IPropsButton> = ({handler}) => {
-
-
-  const {register, handleSubmit, formState: {errors}} = useForm<ICreateBoard>()
-  const onSubmit: SubmitHandler<ICreateBoard> = data => {
-    handler()
+const CreateBoard: FC<IPropsButton> = ({ handler }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ICreateBoard>();
+  const [createBoard, { error }] = api.useCreateBoardMutation();
+  const onSubmit: SubmitHandler<ICreateBoard> = (data) => {
+    console.log(data);
+    const boardInfo: IBoardCreate = {
+      title: `${data.name}|${data.description}`,
+      owner: 'user',
+      users: [],
+    };
+    createBoard(boardInfo);
+    handler();
   };
 
-
-  function submitHandler(e: mouseEvent){
-    e.preventDefault()
-    console.log(e.target)
+  function submitHandler(e: mouseEvent) {
+    e.preventDefault();
+    console.log(e.target);
   }
 
-  return ( 
+  return (
     <div className={styles.modal} onClick={handler}>
-      <form onSubmit={handleSubmit(onSubmit)} onClick={e => e.stopPropagation()} className={styles.boardCreation}>
-        <Typography 
-          variant='h5'
-          align="center" 
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        onClick={(e) => e.stopPropagation()}
+        className={styles.boardCreation}>
+        <Typography
+          variant="h5"
+          align="center"
           sx={{
-            color: "black",
-            marginBottom: "10px",
-          }}
-          >
+            color: 'black',
+            marginBottom: '10px',
+          }}>
           Add Board
         </Typography>
 
         <div className={styles.inputs}>
-          <TextField 
-            {...register("name", {required: true})}
+          <TextField
+            {...register('name', { required: true })}
             name="name"
-            label='board name' 
-            variant="outlined" 
+            label="board name"
+            variant="outlined"
             autoFocus={true}
             error={errors.name && true}
           />
-          {errors.name && <Typography variant="subtitle2" sx={{color: 'red'}}>name is required</Typography>}
+          {errors.name && (
+            <Typography variant="subtitle2" sx={{ color: 'red' }}>
+              name is required
+            </Typography>
+          )}
           <TextField
-            {...register("description", {required: true})}
+            {...register('description', { required: true })}
             name="description"
-            label='board description'
+            label="board description"
             variant="outlined"
             multiline={true}
-            minRows='3'
+            minRows="3"
             error={errors.description && true}
           />
-          {errors.description && <Typography variant="subtitle2" sx={{color: 'red'}}>description is required</Typography>}
+          {errors.description && (
+            <Typography variant="subtitle2" sx={{ color: 'red' }}>
+              description is required
+            </Typography>
+          )}
         </div>
         <div className={styles.buttons}>
-          <Button variant="text" color='success' type="submit" >submit</Button>
-          <Button variant="text" color='warning' onClick={handler}>cancel</Button>
+          <Button variant="text" color="success" type="submit">
+            submit
+          </Button>
+          <Button variant="text" color="warning" onClick={handler}>
+            cancel
+          </Button>
         </div>
       </form>
     </div>
-   );
-}
- 
+  );
+};
+
 export default CreateBoard;
